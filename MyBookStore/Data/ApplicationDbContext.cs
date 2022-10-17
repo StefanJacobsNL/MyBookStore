7dd83511ad8ba00f89eff83850eae1f7fc35a1fd
Stefan Jacobs <stefanjacobsrips@outlook.com>
2022-10-17 10:49:28 +0200

    First Class DB setup

--- MyBookStore/Data/ApplicationDbContext.cs
+++ MyBookStore/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using MyBookStore.Models;
 
 namespace MyBookStore.Data
 {
@@ -9,5 +10,7 @@ namespace MyBookStore.Data
             : base(options)
         {
         }
+
+        public DbSet<Book> Book { get; set; }
     }
 }
---
+++ MyBookStore/Models/Book.cs
@@ -0,0 +1,7 @@
+﻿namespace MyBookStore.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+    }
+}
