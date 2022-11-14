c4b6dac4584b6e4bc1c619e85f19c16fbce6b9be
Stefan Jacobs <stefanjacobsrips@outlook.com>
2022-11-14 16:15:01 +0100

    Created the warehouse db classes

---
+++ MyBookstore.Database/Entities/WarehouseBookDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBookstore.Database.Entities
+{
+    public class WarehouseBookDTO
+    {
+        public int Id { get; set; }
+        public WarehouseDTO Warehouse { get; set; }
+        public int WarehouseId { get; set; }
+        public BookDTO Book { get; set; }
+        public int BookId { get; set; }
+        public int Amount { get; set; }
+
+        public WarehouseBookDTO()
+        {
+
+        }
+
+        public WarehouseBookDTO(int warehouseId, int bookId)
+        {
+            WarehouseId = warehouseId;
+            BookId = bookId;
+        }
+    }
+}
